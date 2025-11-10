4ee2cdb4169cbad44ddea16758c4257d0d24e15f
Matt Ge <mattyige@gmail.com>
2025-11-10 15:39:16 -0500

    adding waitgroups, flags, and fixing relative url issues

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
 	_ "github.com/lib/pq"
@@ -47,9 +48,22 @@ func main() {
 	queue := make(chan string, 2000)
 	discovery := make(chan string, 2000)
 	workers := flag.Int("w", 1, "num web crawlers")
+	duration := flag.Int("d", 20, "duration of crawling")
+	verbose := flag.Bool("v", false, "explicit logs")
+	output := flag.String("o", "db.log", "redirect logs")
 	flag.Parse()
+
+	logFile, err := os.OpenFile(*output, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println("Error logging contents")
+	}
+	defer logFile.Close()
+	log.SetOutput(logFile)
+
 	fmt.Println(*workers, "workers starting...")
 	discovery <- flag.Args()[0]
+
+	var wg sync.WaitGroup
 	go func(q chan string, d chan string) {
 		for job := range d {
 			q <- job
@@ -57,6 +71,7 @@ func main() {
 	}(queue, discovery)
 
 	entries := make(chan page, 2000)
+
 	go func(en chan page, db *sql.DB) {
 		statement := `
 			INSERT INTO pages (url, content)
@@ -65,50 +80,46 @@ func main() {
 		for e := range en {
 			_, err := db.Exec(statement, e.url, e.content)
 			if err != nil {
-				log.Println("Error adding url:", e.url, "into postgres")
+				log.Println("Error adding url:", e.url, "into postgres:", err)
 			}
 		}
 	}(entries, db)
-	for i := range *workers {
-		go func(q chan string, d chan string, e chan page, id int) {
-			for job := range q {
-				Scrape(job, AGENT, d, e, id)
-			}
-		}(queue, discovery, entries, i+1)
-	}
-	for {
-		time.Sleep(time.Second * 15)
-		rows, err := db.Query("SELECT url, crawled_at from pages")
-		if err != nil {
-			log.Println("Error getting entries:", err)
-		}
-		defer rows.Close()
-		var url string
-		var crawled_at time.Time
-		logFile, err := os.OpenFile("db.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			log.Println("Error logging contents")
-		}
-		defer logFile.Close()
-		log.SetOutput(logFile)
-		for rows.Next() {
-			if err = rows.Scan(&url, &crawled_at); err != nil {
-				log.Println("Error scanning db rows:", err)
-			}
-			log.Println("db dump: -url:", url, "-crawled_at:", crawled_at)
 
-		}
+	for i := range *workers {
+		wg.Add(1)
+		go func(id int, q chan string, d chan string, e chan page, db *sql.DB) {
+			defer wg.Done()
+			for job := range q {
+				err = Scrape(id, job, AGENT, d, e, db, *verbose)
+				if err != nil {
+					log.Println(err)
+				}
+			}
+		}(i+1, queue, discovery, entries, db)
 	}
+	time.Sleep(time.Second * time.Duration(*duration))
+	close(entries)
+	close(queue)
+	close(discovery)
+	wg.Wait()
+	fmt.Println("Crawling finished")
 }
 
-func Scrape(Url string, agent string, d chan<- string, e chan page, id int) error {
-	fmt.Println("Waiting on robots.txt.. Worker", id)
+func Scrape(id int, Url string, agent string, d chan<- string, e chan page, db *sql.DB, v bool) error {
+	if v {
+		fmt.Println("Waiting on robots.txt.. Worker", id)
+	}
 	err := robots(Url, agent)
 	if err != nil {
 		return err
 	}
-	fmt.Println("Robots allowed")
-
+	if v {
+		fmt.Println("Robots allowed... Worker", id)
+	}
+	base, err := url.Parse(Url)
+	if err != nil {
+		return err
+	}
 	req, err := http.NewRequest(http.MethodGet, Url, nil)
 	if err != nil {
 		return err
@@ -139,20 +150,41 @@ func Scrape(Url string, agent string, d chan<- string, e chan page, id int) erro
 			case "a":
 				for _, attr := range tok.Attr {
 					if attr.Key == "href" {
-						fmt.Println("Link:", attr.Val)
+						rel := attr.Val
+						r, err := url.Parse(rel)
+						if err != nil {
+							return err
+						}
+						u := base.ResolveReference(r)
+						fmt.Println("Link:", u)
+						check := `SELECT EXISTS (SELECT 1 from pages WHERE url=$1)`
+						var exists bool
+						err = db.QueryRow(check, u.String()).Scan(&exists)
+						if err != nil {
+							log.Println("error checking link existence, -link:", u)
+							return err
+						}
+						if exists {
+							log.Println("url exists, -link:", u, "going ahead")
+							continue
+						}
 						select {
-						case d <- attr.Val:
-							fmt.Println("Sent successfully")
+						case d <- u.String():
+							if v {
+								fmt.Println("Sent successfully: -url:", u.String())
+							}
+							log.Println("Sent successfully: -url:", u.String())
 						default:
-							fmt.Println("Channel full, moving ahead")
+							if v {
+								fmt.Println("Channel full, moving ahead")
+							}
+							log.Println("Channel full, moving ahead")
 						}
 					}
 				}
 			case "script":
 				token_state.depth++
 			case "style":
-				token_state.depth++
-			case "iframe":
 				token_state.depth++
 			}
 		case html.EndTagToken:
@@ -161,8 +193,6 @@ func Scrape(Url string, agent string, d chan<- string, e chan page, id int) erro
 			case "script":
 				token_state.depth--
 			case "style":
-				token_state.depth--
-			case "iframe":
 				token_state.depth--
 			}
 		case html.TextToken:
